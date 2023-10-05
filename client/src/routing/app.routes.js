1423a986f460411c0ced43d6e08005c8f89eed97
CryptoLorn <lorn.crypto@gmail.com>
2023-10-05 16:09:30 +0300

    Initial commit

---
+++ client/src/routing/app.routes.js
@@ -0,0 +1,5 @@
+export const AppRoutes = {
+    HOME: "/",
+    REGISTER: "/register",
+    LOGIN: "/login"
+}
---
+++ client/src/routing/router.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import Layout from '../layouts/Layout';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        // children: [
+        //     {
+        //         index: true,
+        //         path: '/',
+        //         element: <Episodes/>,
+        //     },
+        //     {
+        //         path: '/characters',
+        //         element: <Characters/>,
+        //     }
+        // ]
+    },
+])
+
+export {router}
