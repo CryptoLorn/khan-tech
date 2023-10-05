06f75da8d9e1fa72d1920becfc0be7fecc863444
CryptoLorn <lorn.crypto@gmail.com>
2023-10-05 17:31:02 +0300

    create header

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './Header.css';
+import logo from '../../img/logo.png';
+
+const Header = () => {
+    return (
+        <div className={'header_wrapper'}>
+            <div className={'header'}>
+                <div className={'header_left_block'}>
+                    <div><img src={logo} alt={'logo'} /></div>
+                    <div>
+                        <div className={'header_help_text'}>Need Help?</div>
+                        <div className={'header_help_phone'}>(514) 543-9936</div>
+                    </div>
+                </div>
+
+                <div className={'header_right_block'}>
+                    <div className={'header_right_block_menu'}>Home</div>
+                    <div className={'header_right_block_menu'}>Services</div>
+                    <div className={'header_right_block_menu'}>About</div>
+                    <div className={'header_right_block_menu'}>Book now</div>
+                    <div className={'header_right_block_menu'}>Shop</div>
+                    <div className={'header_right_block_menu'}>Blog</div>
+                    <div className={'header_right_block_menu'}>Contact</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
