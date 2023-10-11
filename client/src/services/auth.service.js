050612f530157bf1929fcb9c30e9de356b92fefc
CryptoLorn <lorn.crypto@gmail.com>
2023-10-11 08:45:14 +0300

    delete comments

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,19 +1,11 @@
 import { axiosService } from './axios.service';
 import { urls } from '../configs/urls';
-import { USER } from '../constants/role.enum';
 import { ACCESS_TOKEN } from '../constants/token.enum';
 
 export const authService = {
     login: (email, password) => axiosService
         .post(urls.login, {email, password})
         .then(value => value.data),
-
-    registration: (email, password) => axiosService.post(urls.registration, {email, password, role: USER})
-        .then(value => {
-            localStorage.setItem(ACCESS_TOKEN, value.data.access_token);
-            return value.data.user;
-        }
-    ),
 
     checkIsAuth: () => axiosService.get(urls.refresh).then(value => value.data),
 
